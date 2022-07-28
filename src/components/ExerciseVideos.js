5ad429a32035eac609659c9f7a01d2a63fb77f98
Anika Angi <anika.angi@gmail.com>
2022-07-28 13:57:46 -0500

    final appearance changes

--- src/components/ExerciseVideos.js
+++ src/components/ExerciseVideos.js
@@ -11,7 +11,7 @@ const ExerciseVideos = ({exerciseVideos, name}) => {
       <Typography variant='h4' fontWeight='bold' mb='33px'>
         Watch {` `} <span style={{ color: '#ff2625', textTransform:'capitalize'}}>{name}</span> {` `} Exercise Videos
       </Typography>
-      <Stack justifyContent='flex-start' flexWrap='wrap' alignItems='center'
+      <Stack justifyContent='flex-start' flexWrap='wrap' alignItems='top'
       sx={{flexDirection: { lg: 'row'}, gap: { lg: '110px', xs: '0px'}}}>
         {exerciseVideos?.slice(0, 3).map((item, index) => (
           <a key={index}
@@ -19,12 +19,15 @@ const ExerciseVideos = ({exerciseVideos, name}) => {
           href={`https://www.youtube.com/watch?v=${item.video.videoId}`}
           target='_blank'
           rel='noreferrer'
-          style={{maxWidth:'400px'}}
+          style={{maxWidth:'400px', textDecoration:'none'}}
+          text-decoration='none'
+          
           >
             <img src={item.video.thumbnails[0].url} alt={item.video.title} 
             style={{maxWidth:'350px', maxHeight:'200px', margin:'0' }}/>
             <Box>
-              <Typography sx={{ fontSize: { lg: '18px', xs: '12px' } }} fontWeight={600} color='#000'>
+              <Typography sx={{ fontSize: { lg: '18px', xs: '12px' } }} fontWeight={600} color='#000'
+              >
                 {item.video.title}
               </Typography>
               <Typography fontSize="14px" color='#000'>
